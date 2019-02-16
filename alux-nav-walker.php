636edb9f7fd2b8ad1c9cfc0d32084d6372afe757
Brendy1989 <47702628+Brendy1989@users.noreply.github.com>
2019-02-16 23:58:20 +0100

    Add files via upload

---
+++ alux-nav-walker.php
@@ -0,0 +1,85 @@
+<?php
+ /*
+ * Custom walker class.
+ */
+class alux_nav_walker extends Walker_Nav_Menu {
+
+    /*
+     * Starts the list before the elements are added.
+     *
+     * Adds classes to the unordered list sub-menus.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     */
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        // Depth-dependent classes.
+        $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+        $display_depth = ( $depth + 1); // because it counts the first submenu as 0
+        $classes = array(
+          '',
+          ( $display_depth == 0  ? '' : 'dropdown-menu' ),
+          ( $display_depth >=2 ? 'sub-sub-menu' : '' ),
+          'menu-depth-' . $display_depth
+        );
+        $class_names = implode( ' ', $classes );
+
+        // Build HTML for output.
+        $output .= "\n" . $indent . '<ul class="' . $class_names . '">' . "\n";
+    }
+
+    /**
+     * Start the element output.
+     *
+     * Adds main/sub-classes to the list items and links.
+     *
+     * @param string $output Passed by reference. Used to append additional content.
+     * @param object $item   Menu item data object.
+     * @param int    $depth  Depth of menu item. Used for padding.
+     * @param array  $args   An array of arguments. @see wp_nav_menu()
+     * @param int    $id     Current item ID.
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        global $wp_query;
+        $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+
+        // Depth-dependent classes.
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+        $depth_classes = array(
+            /*( $depth == 0 ? 'dropdown' : '' ),*/
+            (in_array('menu-item-has-children', $classes) ? 'dropdown' : ''),
+
+            'menu-item-depth-' . $depth
+
+        );
+        $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+
+        // Passed classes.
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+
+        // Build HTML.
+        $output .= $indent . '<li '. ' class="' . $depth_class_names . ' ' . $class_names . '">';
+
+        // Link attributes.
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $attributes .=  ( in_array('menu-item-has-children', $classes) ? 'data-toggle="dropdown"' : '' ) . ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+
+        // Build HTML output and pass through the proper filter.
+        $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s'.( in_array('menu-item-has-children', $classes) ? '<div class="alux-arrow animate"></div>' : '' ). '</a>%6$s',
+            $args->before,
+            $attributes,
+            $args->link_before,
+            apply_filters( 'the_title', $item->title, $item->ID ),
+            $args->link_after,
+            $args->after
+        );
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
